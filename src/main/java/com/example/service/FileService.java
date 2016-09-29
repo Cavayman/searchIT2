94ceed5e32f128dc7c01ad795d23be019ea942f4
Cavayman <cavayman@gmail.com>
2016-09-29 14:11:39 +0300

    first commit

---
+++ src/main/java/com/example/service/FileService.java
@@ -0,0 +1,7 @@
+package main.java.com.example.service;
+
+/**
+ * Created by cavayman on 29.09.2016.
+ */
+public class FileService {
+}
---
+++ src/main/java/com/example/web/WelcomeServlet.java
@@ -0,0 +1,30 @@
+package main.java.com.example.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by cavayman on 29.09.2016.
+ */
+@WebServlet(value = "/")
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h2>Hello World! Today is " + new java.util.Date() + "</h2></body></html>");
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
