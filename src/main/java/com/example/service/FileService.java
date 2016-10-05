bcfc6098a8a062b6b60c529e469265f2a0c07bc7
Cavayman <cavayman@gmail.com>
2016-10-05 07:46:58 +0300

    implemented converter from Byto to KB,improved saving file strategy

--- src/main/java/com/example/service/FileService.java
+++ src/main/java/com/example/service/FileService.java
@@ -1,34 +1,57 @@
 package com.example.service;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+//import com.example.util.PropertyGetterSingleton;
+//import org.json.Property;
+//import org.json.simple.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import com.example.repository.dao.MetadataDAO;
+import com.example.repository.dao.impl.MetadataDAOImpl;
+import com.example.repository.model.Metadata;
+
+import javax.servlet.http.Part;
+import java.io.*;
+import java.util.Date;
+import java.util.Formatter;
 
 /**
  * Created by cavayman on 29.09.2016.
  */
 public class FileService {
 
-    public static File file;
-
-    public String getTextFile(File file){
-        String resoult="";
-        try (BufferedReader br = new BufferedReader(new FileReader(file)))
-        {
+    private MetadataDAOImpl metadataDAO=new MetadataDAOImpl(Metadata.class);
+//    delete this. duplicated method with  JSON Maked readFile
+    public String getTextFile(String fileName) {
+        StringBuilder content = new StringBuilder();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String sCurrentLine;
-
             while ((sCurrentLine = br.readLine()) != null) {
-            resoult+=sCurrentLine;
+                content.append(sCurrentLine);
             }
-        return resoult;
+            return content.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return resoult;
+        return content.toString();
     }
+
+    public void saveTextFile(Part part) throws IOException {
+        part.write(part.getSubmittedFileName());
+        saveMetadata( new File(part.getSubmittedFileName()));
+    }
+
+    private void saveMetadata(File savedFile) {
+        Metadata metadata = new Metadata();
+        metadata.setFileName(savedFile.getName());
+        metadata.setFileSize(convertFromByteToKB(savedFile.length()));
+        metadata.setFileCreationDate(new Date(savedFile.lastModified()));
+        metadata.setFileLastModifiedDate(new Date(savedFile.lastModified()));
+        metadataDAO.save(metadata);
+    }
+
+    private String convertFromByteToKB(Long byteSize) {
+        double kilobytes = (byteSize / 1024);
+        return String.format("%(.1f KB", kilobytes);
+    }
+    
 }
